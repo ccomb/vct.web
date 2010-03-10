f8c9848345a0f16647e3d79420fcfee3385f586b
Christophe Combelles <ccomb@free.fr>
2010-03-10 11:36:16 +0100

    unversion Data.fs, changed the root model, turned routes into views

--- vctdemo/models.py
+++ vctdemo/models.py
@@ -1,11 +1,11 @@
 from persistent.mapping import PersistentMapping
 
-class MyModel(PersistentMapping):
+class VctRoot(PersistentMapping):
     __parent__ = __name__ = None
 
 def appmaker(zodb_root):
     if not 'app_root' in zodb_root:
-        app_root = MyModel()
+        app_root = VctRoot()
         zodb_root['app_root'] = app_root
         import transaction
         transaction.commit()
