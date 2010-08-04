38aa284fb122e95545397ce858d1dc611ad5056e
Christophe Combelles <ccomb@free.fr>
2010-08-04 16:41:00 +0200

    fixed main template + style

--- vctdemo/views/__init__.py
+++ vctdemo/views/__init__.py
@@ -5,6 +5,3 @@ from formalchemy.ext.zope import FieldSet
 from fa.jquery import renderers as jquery
 FieldSet.default_renderers.update(jquery.default_renderers)
 
-from formalchemy import config
-from fa.jquery.utils import TemplateEngine
-config.engine = TemplateEngine()
