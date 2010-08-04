0dc443265bb2358629d7ef9d382c0cc24bd6f79d
Christophe Combelles <ccomb@free.fr>
2010-08-04 16:31:30 +0200

    only 1 jqueryui theme

--- vctdemo/views/__init__.py
+++ vctdemo/views/__init__.py
@@ -1,1 +1,10 @@
 #
+
+# enable fa.jquery
+from formalchemy.ext.zope import FieldSet
+from fa.jquery import renderers as jquery
+FieldSet.default_renderers.update(jquery.default_renderers)
+
+from formalchemy import config
+from fa.jquery.utils import TemplateEngine
+config.engine = TemplateEngine()
